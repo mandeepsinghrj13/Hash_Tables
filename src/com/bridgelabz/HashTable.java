2baad877c9e9a86567e1154f424b9960bface393
mandeep <singh.mr.1996@gmail.com>
2021-07-09 10:15:57 +0530

    removing avoidable word

--- src/com/bridgelabz/HashTable.java
+++ src/com/bridgelabz/HashTable.java
@@ -1,10 +1,12 @@
 package com.bridgelabz;
+import java.util.Hashtable;
 public class HashTable
 {
 	public static void main(String[] args) 
 	{
 		HashtableImpl<String, Integer> hashImpl = new HashtableImpl();
-        String message = "To be or not to be";
+        String message = "Paranoids are not paranoid because they are paranoid but " +
+                "because they keep putting themselves deliberately into paranoid avoidable situations";
 
         //String to String[] conversion.
         String[] messageArray = message.toLowerCase().split(" ");
@@ -21,6 +23,11 @@ public class HashTable
             hashImpl.add(word , value);
         }
         //Display the linked list
-        System.out.println(hashImpl);	
+        System.out.println(hashImpl);
+        //Remove "avoidable" from the hashtable
+        hashImpl.remove("avoidable");
+
+        //Display the hashtable
+        System.out.println(hashImpl);
 }
 }
--- src/com/bridgelabz/HashtableImpl.java
+++ src/com/bridgelabz/HashtableImpl.java
@@ -82,6 +82,26 @@ public class HashtableImpl<K, V>
         //System.out.println("Key: "+word+" hashcode: "+hashCode+" index: "+index);
         return index;
     }
+    //Remove "avoidable" from hashtable
+    public void remove(K word) 
+    {
+        Node currentNode = head;
+        Node previousNode = null;
+        while (currentNode != null && currentNode.getKey().equals(word)) {
+            head = currentNode.getNext();
+            return;
+        }
+        while (currentNode != null && !(currentNode.getKey().equals(word))) {
+            previousNode = currentNode;
+            currentNode = currentNode.getNext();
+        }
+        if (currentNode != null) 
+        {
+            previousNode.next = currentNode.next;
+        }
+        if(currentNode == null)
+            System.out.println("Word not found!");
+    }
     //Print the linked list
     @Override
     public String toString() 
